c4d719efd3801ea6296d38ae6d4b980f8076c1e5
Sebastian <silbay4160@uta.edu.ec>
2022-12-31 19:42:24 -0500

    se establecio la conexion con la base de datos

---
+++ static/BD/conexion.php
@@ -0,0 +1,15 @@
+
+<?php 
+
+    function conectar(){
+        $servidor="localhost";
+        $usuario="root";
+        $clave="";
+        $baseDatos="proyectomycs";
+
+        $con= new PDO('mysql:host='.$servidor.';dbname='.$baseDatos, $usuario, $clave);
+        return $con;
+
+    }
+
+?>
