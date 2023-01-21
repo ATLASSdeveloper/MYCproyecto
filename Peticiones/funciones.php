f967977b784ecbcb495a5e98d816048574be9893
Sebastian <silbay4160@uta.edu.ec>
2023-01-21 00:00:03 -0500

    Se realizó la complementación entre la funcionalidad y el diseño en paginaPrincipal.php para mostrar todos los productos cargados en la base de datos, además, se agregó la consulta para listar articulos

--- Peticiones/funciones.php
+++ Peticiones/funciones.php
@@ -21,6 +21,38 @@ function impresionControlada(){
     return $acumulador;
 }
 
+function impresionControladaArticulos(){
+  $datos=listarArticulos();
+  $acumulador="";
+  foreach ($datos as $d){
+      $acumulador.=  "<div class='col-md-3'><div class='product'>
+      <div class='product-img'>
+      <img  width='150' height='250' src='data:image/jpg;base64,".base64_encode($d['imagen'])."' />
+        <div class='product-label'>
+          <span class='new'>NEW</span>
+        </div>
+      </div>
+      <div class='product-body'>
+        <p class='product-category'>".$d['marca']."</p>
+        <h3 class='product-name'><a  >".$d['nombre']."</a></h3>
+        <h4 class='product-price'>$".$d['precio']." </h4>
+        <div class='product-rating'>
+          <i class='fa fa-star'></i>
+          <i class='fa fa-star'></i>
+          <i class='fa fa-star'></i>
+          <i class='fa fa-star'></i>
+          <i class='fa fa-star'></i>
+        </div>
+
+      </div>
+      <div class='add-to-cart'>
+        <a href='#' class=' articulos ' id=".$d['id']."><button class='add-to-cart-btn'><i class='fa fa-dollar'></i> Comprar</button></a>
+      </div>
+    </div></div>";
+  }
+  return $acumulador;
+}
+
 function enviarInformacionArticulo($id){
     return detalle_articulos($id);
 }
