124523720c8a03037a676f1c3979df0997c1875a
Andres <andytacuaman13@gmil.com>
2023-01-20 22:15:53 -0500

    Eliminación de los archivos que se clonaron dentro de la carpeta js

--- FrontEnd/static/js/javascript/compra.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".botonConfirmar").click(function(){
-        if(document.getElementById("correo").value=="" ||document.getElementById("clave").value=="" ){
-            document.getElementById("verifica").hidden=false;
-        }else{
-            window.location.href="../factura.html";
-        }
-    })
-})
